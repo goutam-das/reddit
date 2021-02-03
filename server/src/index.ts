e303bb2f9f7909b25fa0c8b68e75d198e7a1b36c
Goutam Das <goutamdas676@gmail.com>
2021-02-04 00:32:48 +0530

    morgan added

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,20 +2,27 @@ import 'reflect-metadata';
 import 'dotenv/config';
 import express, { Request, Response } from 'express';
 import { createConnection } from 'typeorm';
+import morgan from 'morgan';
 
 (async () => {
+
     try {
         await createConnection();
         console.log('DB connected');
         const app = express();
+
         // Middlewares
         app.use(express.json());
+        app.use(morgan('dev'));
+
         app.get('/', (_: Request, res: Response) => {
             return res.send('Hello from Server');
-        })
+        });
+
         app.listen(process.env.PORT);
         console.log(`Server listen on port: ${process.env.PORT}`);
     } catch (error) {
         console.error(error);
     }
+    
 })();
